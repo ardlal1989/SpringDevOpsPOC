9166e642f65c7ddff875588262b5ccb9b4bb87ea
mart3051 <mart3051@gmail.com>
2020-10-17 14:28:36 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-nohup java -jar -Dserver.port=9000 target/${NAME}-${VERSION}.jar &
+java -jar -Dserver.port=9000 target/${NAME}-${VERSION}.jar &>log & disown;
