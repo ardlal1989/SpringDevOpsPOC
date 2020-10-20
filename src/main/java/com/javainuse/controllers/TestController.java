88bc5ecde7a3bbe6d5887177d0f9214d9358dcd2
Ardhendu Lal <addy.1989.1aug@gmail.com>
2020-10-20 20:23:19 +0530

    Added HTML files

--- src/main/java/com/javainuse/controllers/TestController.java
+++ src/main/java/com/javainuse/controllers/TestController.java
@@ -1,24 +1,28 @@
 package com.javainuse.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javainuse.model.Employee;
 
-@RestController
+@Controller
 public class TestController {
 
 	@RequestMapping(value = "/employee", method = RequestMethod.GET)
-	public Employee firstPage() {
+	@ResponseBody
+	public Employee firstPage(Employee employee) {
 
-		Employee emp = new Employee();
-		emp.setName("emp1");
-		emp.setDesignation("manager");
-		emp.setEmpId("1");
-		emp.setSalary(10000);
-
-		return emp;
+		
+		return employee;
 	}
+	@RequestMapping(value = "/")
+	public String hello()
+	{
+		return "index";
+	}
+	
 
 }
