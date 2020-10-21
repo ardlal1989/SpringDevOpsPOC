afe9ee367516a4bc5625376c9fb81b6bb084051b
ardlal1989 <50587555+ardlal1989@users.noreply.github.com>
2020-10-21 19:24:45 +0530

    Update TestWebApp.java

--- src/test/java/com/javainuse/test/TestWebApp.java
+++ src/test/java/com/javainuse/test/TestWebApp.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
